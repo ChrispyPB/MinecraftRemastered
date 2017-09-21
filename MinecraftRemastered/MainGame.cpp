86d8226786736198b537f27ee95cb79c820959cc
Christoph Pader-Barosch <christoph.pader-barosch@hotmail.com>
2017-09-21 22:11:20 +0200

    Added additional initialization

--- MinecraftRemastered/MainGame.cpp
+++ MinecraftRemastered/MainGame.cpp
@@ -36,7 +36,24 @@ void MainGame::initSystems() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	_window = SDL_CreateWindow("Minecraft Remastered", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _windowWidth, _windowHeight, SDL_WINDOW_OPENGL);
-	
+
+	if (_window == nullptr)
+		fatalError("SDL Window could not be created!");
+
+	// Create SDL_GL context
+	SDL_GLContext glContext = SDL_GL_CreateContext(_window);
+	if (glContext == nullptr)
+		fatalError("SDL_GL context could not be created!");
+
+	// Initialize glew
+	GLenum error = glewInit();
+	if (error != GLEW_OK)
+		fatalError("Could not initialize glew!");
+
+	// Activate Double Buffer
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 }
 
 void MainGame::gameLoop() {
